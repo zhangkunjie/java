29a6de4a7ea81cc7105967d27f47b90ef5c55b40
kunjie.zhang <kunjie.zhang@corp.elong.com>
2018-05-09 09:45:44 +0800

    test

---
+++ src/main/java/com/java/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.java.basic;
+
+/**
+ * Created by kunjie.zhang on 2018/5/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
